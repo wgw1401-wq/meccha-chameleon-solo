37a2f7073510b058d4685e3785140fb944fee3d7
Meccha Chameleon <meccha-chameleon@users.noreply.github.com>
2026-08-12 15:34:45 +0900

    Fix inactive lobby buttons on static URL

--- public/game3d.js
+++ public/game3d.js
@@ -1,4 +1,4 @@
-import * as THREE from "/vendor/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.module.js";
 
 const $ = s => document.querySelector(s);
 const canvas = $("#gameCanvas"), menu = $("#menu"), hud = $("#hud"), crosshair = $("#crosshair"), colorPanel = $("#colorPanel");
